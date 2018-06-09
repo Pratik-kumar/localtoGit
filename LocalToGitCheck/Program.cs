23cb71a5b8d0b838712cbd68313d4ab0f9139cf3
Pratik <pratik.kumarpsc@gmail.com>
2018-06-09 20:01:20 +0530

    Local To git Check

---
+++ LocalToGitCheck/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalToGitCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating a git hub repo from code existing in local machine");
+            //source :- https://www.youtube.com/watch?v=HVsySz-h9r4&t=1072s
+            Console.ReadLine();
+        }
+    }
+}
